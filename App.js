cb82d3fd3172bea1d54b7114cd0ed4abbe4b8089
ViestursK <95314459+ViestursK@users.noreply.github.com>
2021-12-08 23:04:04 +0200

    Updated app, added ListView

--- App.js
+++ App.js
@@ -1,62 +1,102 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import React, { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { FlatList, StyleSheet, Text, View, Button } from "react-native";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const MyStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "React application" }}
+        />
+        <Stack.Screen name="List View" component={FlatListBasics} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const HomeScreen = ({ navigation }) => {
+  const [count, setCount] = useState(0);
   return (
     <View style={styles.container}>
-      <Image
-        source={require("./assets/640px-ICloud_logo.svg.png")}
-        style={styles.imageStyle}
-      ></Image>
-      <Text style={styles.heading}>Hello world!</Text>
-      <Text style={styles.paragraph}>
-        Hello world from group 2! Member:{" "}
-        <Text style={{ fontWeight: "700" }}>Viesturs Kalcenaus</Text>
-      </Text>
-      <Text style={styles.paragraph}>
-        This is my Viesturs Kalcenaus, first React Native application! Nice to
-        see You here!
-      </Text>
-
-      <Image
-        source={require("./assets/shrek.png")}
-        style={styles.roundImageStyle}
-      ></Image>
-      <Text style={styles.heading}>Viesturs Kalcenaus</Text>
-      <StatusBar style="auto" />
+      <View style={styles.goBtn}>
+        <Button
+          title="Go to List View"
+          onPress={() => navigation.navigate("List View")}
+        />
+      </View>
+      <View style={styles.btn}>
+        <Button title="Increase count" onPress={() => setCount(count + 1)} />
+      </View>
+      <View style={styles.btn}>
+        <Button title="Decrease count" onPress={() => setCount(count - 1)} />
+      </View>
+      <View>
+        <Text> Current count: {count}</Text>
+        <Text style={styles.line}>
+          ___________________________________________________
+        </Text>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
+  goBtn: {
+    marginBottom: 30, 
+    marginLeft: 15,
+    marginRight: 15,
+  },
+  btn: {
+    margin: 15,
+  },
+  line: {
+    textAlign: "center",
+    color: "grey",
+  },
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 22,
   },
-  imageStyle: {
-    height: 70,
-    width: 110,
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
-  roundImageStyle: {
-    borderRadius: 200,
-    width: 100,
-    height: 100,
-  },
-  heading: {
-    fontSize: 16,
-    color: "grey",
-    fontWeight: "700",
+  itemCont: {
+    backgroundColor: "lightgrey",
     padding: 20,
-  },
-  paragraph: {
-    marginRight: 50,
-    marginLeft: 50,
-    alignContent: "flex-start",
-    fontSize: 14,
-    color: "grey",
-    padding: 20,
+    borderRadius: 5,
+    margin: 8,
   },
 });
+
+const list = [
+  { name: "John Doe 1" },
+  { name: "John Doe 2" },
+  { name: "John Doe 3" },
+  { name: "John Doe 4" },
+  { name: "John Doe 5" },
+];
+
+const FlatListBasics = () => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={list}
+        renderItem={({ item }) => (
+          <View style={styles.itemCont}>
+            <Text style={styles.item}>{item.name}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+export default MyStack;
