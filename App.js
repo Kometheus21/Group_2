c1d6509952ee01811a0815e7a9a7c3f6dc9c8667
woodis420 <95314339+woodis420@users.noreply.github.com>
2021-12-08 23:39:34 +0200

    Kodc

--- App.js
+++ App.js
@@ -1,20 +1,98 @@
-import React from 'react';
-import {Image, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { FlatList, StyleSheet, Text, View, Button } from "react-native";
 
-const HelloWorldApp = () => {
+const Stack = createNativeStackNavigator();
+
+const MyStack = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-      }}>
-            <Image source={{ uri: "https://p.favim.com/orig/2018/07/13/alien-drunk-smoke-Favim.com-6035351.jpg" }} style={{ width: 200, height: 360 }} />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home "
+          component={HomeScreen}
+          options={{ title: "React application" }}
+        />
+        <Stack.Screen name="List View" component={FlatListBasics} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
-      <Text>Hello world! from Group Nr.2</Text>
-      <Text>This is Rudolfs Andrejs Andersons</Text>
-      <Text>and it's my 1st react native application wow</Text>
+const HomeScreen = ({ navigation }) => {
+  const [count, setCount] = useState(0);
+  return (
+    <View style={styles.container}>
+      <View style={styles.button}>
+        <Button
+          title="Go to List View"  color = "green"
+          onPress={() => navigation.navigate("List View")}
+        />
+      </View>
+      <View style={styles.button}>
+        <Button title="increase count"  color = "green" onPress={() => setCount(count + 1)} />
+      </View>
+      <View style={styles.button}>
+        <Button title="decrease count" color = "green" onPress={() => setCount(count - 1)} />
+      </View>
+      <View>
+        <Text> Current count: {count}</Text>
+        <Text style={styles.decor}>
+          ...................................................................
+        </Text>
+      </View>
     </View>
-  )
-}
-export default HelloWorldApp;
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    margin : 15,
+  },
+  
+  decor: {
+    textAlign: "center",
+    color: "green",
+  },
+  container: {
+    flex: 1,
+    paddingTop: 40,
+  },
+  item: {
+    padding: 14,
+    fontSize: 25,
+    color : "white",
+  },
+  itemCont: {
+    backgroundColor: "grey",
+    padding: 5,
+    borderRadius: 4,
+    margin: 8,
+  },
+});
+
+const list = [
+  { name: "John Doe 1" },
+  { name: "John Doe 2" },
+  { name: "John Doe 3" },
+  { name: "John Doe 4" },
+  { name: "John Doe 5" },
+];
+
+const FlatListBasics = () => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={list}
+        renderItem={({ item }) => (
+          <View style={styles.itemCont}>
+            <Text style={styles.item}>{item.name}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+export default MyStack;
